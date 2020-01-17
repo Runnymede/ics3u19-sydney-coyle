3735059d45f9a52f35e67cf88b2714368b238e73
333550275 <333550275@C-4JNJVR1.tdsb.on.ca>
2020-01-17 13:43:49 -0500

    simon again

--- ICS3U-Sydney/src/Culminating/Simon.java
+++ ICS3U-Sydney/src/Culminating/Simon.java
@@ -8,6 +8,7 @@ public class Simon {
 
 
 	static ArrayList<Character> pattern = new ArrayList<Character>();
+	static String guess;
 
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Welcome to Simon!\nIn this game, the computer will display a series of letters.");
@@ -15,49 +16,56 @@ public class Simon {
 		System.out.println("If you get the pattern right you may move on to the next level.");
 		System.out.println("However, if you get it wrong, you will be sent back to level 1.");
 		System.out.println("Pass level 10 to win the game!");
-		
-		levelNum(level,guess);
+
+		levelNum(guess);
+		createPattern();
 		printPattern();
+		getAnswer(guess);
+		checkAnswer(guess);
 	}
-	public static int levelNum(int level,String guess) {
-		level++;
+	//
+	public static int levelNum(String guess) {
+		int level = 1;
 		if(checkAnswer(guess)==false) 
 			level = 1;
-		
+
 		System.out.println("Level " +level);
-		
+
 		if (level>10) 
 			System.out.println("Wow! you beat level 10!\n You win!");
 
 		return level;
 	}
-	public static char createPattern(int x, String guess, int level) {
+	//
+	public static char createPattern() {
 		int num = 0;
 		pattern.clear();
 
-		for(int i = 0; i <= levelNum(level,guess); i++) {
-			num = (int) (Math.random()*122)+97;
+		for(int i = 0; i <10; i++) {
+			num = (int) (Math.random());
 			pattern.add((char) num);
 		}
 
 		return (char)num;
 	}
-
+	//
 	public static void printPattern() throws InterruptedException{
 		for (int i = 0; i <pattern.size(); i++){
-			System.out.println (pattern.get(i));
+			System.out.println(pattern.get(i));
 			Thread.sleep(1000);
 		}
 
 	}
-	public static String getAnswer(String guess) {
+	//
+	public static String getAnswer(String x) {
 		Scanner sc = new Scanner (System.in);
-		System.out.println("your guess:");
+		System.out.println("your answer:");
 		guess = sc.next();
 		sc.close();
 		return guess;
 	}
-	public static boolean checkAnswer(String guess) {
+	//
+	public static boolean checkAnswer(String x) {
 		for(int i = 0; i < pattern.size(); i++) {
 			if(guess.charAt(i)!=pattern.get(i)) 
 				return false;	
