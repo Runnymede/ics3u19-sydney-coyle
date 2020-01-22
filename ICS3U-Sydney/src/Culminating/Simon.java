7b3b392329a30a5c808d5d95654390dab9645c09
333550275 <333550275@C-4JNJVR1.tdsb.on.ca>
2020-01-22 15:19:46 -0500

    Simon Final

--- ICS3U-Sydney/src/Culminating/Simon.java
+++ ICS3U-Sydney/src/Culminating/Simon.java
@@ -1,75 +1,125 @@
 package Culminating;
 
-import java.util.Scanner;
+import hsa_new.Console;
 import java.util.ArrayList;
-
+/**
+ * Simon.java <br>
+ * Displays letter(s) on the screen for one second then clears it
+ * and waits for the user to type in the same letter(s) 
+ * then tells the user if they are right or not<br>
+ * January 10, 2020
+ * @author Sydney Coyle
+ */
 
 public class Simon {
 
-
 	static ArrayList<Character> pattern = new ArrayList<Character>();
 	static String guess;
+	static int level;
+	static int again;
 
+	static Console c = new Console ();
+	
+	/** 
+	 *Entry to the program
+	 *@param args unused
+	 */
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("Welcome to Simon!\nIn this game, the computer will display a series of letters.");
-		System.out.println("Your job is to type in the exact same letters in the exact same order.");
-		System.out.println("If you get the pattern right you may move on to the next level.");
-		System.out.println("However, if you get it wrong, you will be sent back to level 1.");
-		System.out.println("Pass level 10 to win the game!");
+		//introduction
+		c.println("Welcome to Simon!\nIn this game, the computer will display a series of letters.");
+		c.println("Your job is to type in the exact same letters in the exact same order.");
+		c.println("If you get the pattern right you may move on to the next level.");
+		c.println("However, if you get it wrong, you will be sent back to level 1.");
+		c.println("Pass level 5 to win the game!");
+		Thread.sleep(1000);
 
-		levelNum(guess);
-		createPattern();
-		printPattern();
-		getAnswer(guess);
-		checkAnswer(guess);
+		level=1;
+		//loops each task every level
+		do {
+			createPattern();
+			printPattern();
+			getAnswer();
+			levelNum();
+		}while(again == 1);
 	}
-	//
-	public static int levelNum(String guess) {
-		int level = 1;
-		if(checkAnswer(guess)==false) 
+
+	/** 
+	 *Keeps track of the game level and determines if they lose
+	 * @throws InterruptedException 
+	 */
+	public static void levelNum() throws InterruptedException {
+		boolean rightAnswer = checkAnswer();
+		//player guesses the right letter
+		if(rightAnswer == true) {	
+			level++;
+			again = 1;
+		}
+		//player guesses the wrong letter
+		else {
 			level = 1;
+			again = 1;
+		}
+		//player wins
+		c.clear();
+		c.println("Level " +level);
+		Thread.sleep(1000);
 
-		System.out.println("Level " +level);
-
-		if (level>10) 
-			System.out.println("Wow! you beat level 10!\n You win!");
-
-		return level;
+		if (level>5) {
+			c.println("Wow! you beat level 5!\nYou win!");
+			again = 0;
+		}
+		
 	}
-	//
-	public static char createPattern() {
-		int num = 0;
+
+	/** 
+	 *generates the random letters used for the pattern
+	 */
+	public static void createPattern() {
+		int character = 0;
 		pattern.clear();
-
-		for(int i = 0; i <10; i++) {
-			num = (int) (Math.random());
-			pattern.add((char) num);
+		for(int i = 0; i <level; i++) {
+			character = (int) (Math.random()*26 + 97);
+			pattern.add((char) character);
 		}
-
-		return (char)num;
 	}
-	//
+
+	/** 
+	 *outputs the pattern once its made
+	 */
 	public static void printPattern() throws InterruptedException{
+		c.clear();
 		for (int i = 0; i <pattern.size(); i++){
-			System.out.println(pattern.get(i));
+			c.println (pattern.get(i));
 			Thread.sleep(1000);
+			c.clear();
 		}
+	}
+	/** 
+	 *gets input from the user
+	 */
+	public static void getAnswer() {
+		c.println("your answer:");
+		guess = c.readLine();
+	}
 
-	}
-	//
-	public static String getAnswer(String x) {
-		Scanner sc = new Scanner (System.in);
-		System.out.println("your answer:");
-		guess = sc.next();
-		sc.close();
-		return guess;
-	}
-	//
-	public static boolean checkAnswer(String x) {
+	/** 
+	 *determines whether the users answer matches the computers or not
+	 *@param guess
+	 *@return if the users answer is correct or not
+	 */
+	public static boolean checkAnswer() {
+		boolean right = false;
 		for(int i = 0; i < pattern.size(); i++) {
-			if(guess.charAt(i)!=pattern.get(i)) 
-				return false;	
+			char answer = guess.charAt(i);
+			//users answer is right
+			if(answer == pattern.get(i)) {
+				right = true;
+			}
+			//users answer is wrong
+			else {
+				return false;
+			}
 		}
-		return true;
+		return right;
 	}
 }
